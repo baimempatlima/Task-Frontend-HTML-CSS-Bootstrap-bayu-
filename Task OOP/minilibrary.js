b856ae994f292b4b5de9a41363c5983747747684
baim empat <baimempatlima@gmail.com>
2022-01-09 12:21:24 +0700

    update Task OOP tugas  minilibrary

--- Task OOP/minilibrary.js
+++ Task OOP/minilibrary.js
@@ -1,27 +1,53 @@
-function funCall() {
-    var data = [
-        {name : 'bayu rizki', alamat: 'jalan jakarta', email : 'baimempatlima@gmail.com'}, 
-        {name : 'rizki', alamat: 'jalan makassar', email : 'rizki1@gmail.com'}
-    ]
-    var html = "<table class='table table-bordered'>";
+class Tabel {
+    constructor(awal){
+        this.awal = awal;
+    }
 
-    setTimeout(() => {
-        html+= '<thead>';
-        html+= '<tr>';
-        html+= '<td>'+'Nama' +'</td>';
-        html+= '<td>'+'Alamat' +'</td>';
-        html+= '<td>'+'Email' +'</td>';
-        html+= '</tr>';
-        html+= '</thead>';
+    headerTabel(data) {
+       var tagpembuka = "<thead><tr>";  
+       var tagpenutup = "</tr></thead>";
 
-        for (var i = 0; i < data.length; i++) {
-            html+= '<tr>';
-            html+= '<td>'+data[i].name+'</td>';
-            html+= '<td>'+data[i].alamat+'</td>';
-            html+= '<td>'+data[i].email+'</td>';
-            html+= '</tr>';
-        }
-        document.getElementById("tab").innerHTML = html
-    });
+       data.forEach(e => {
+           tagpembuka += `<th>${e}</th>`;
+       });
+
+       return tagpembuka + tagpenutup;
+    }
+
+    bodyTabel(data){
+        var tagpembuka = "<tbody>";
+        var tagpenutup = "</tbody>";
+
+        data.forEach((e)=>{
+            tagpembuka += `
+            <tr>
+                <td>${e[0]}</td>
+                <td>${e[1]}</td>
+                <td>${e[2]}</td>
+            </tr>
+            `;
+        });
+        return tagpembuka + tagpenutup;
+    }
+
+    render(elemen){
+        var html = 
+        "<table class='table table-bordered'>" + this.headerTabel(this.awal.judulTabel) + this.bodyTabel(this.awal.data) + "</table>";
+        elemen.innerHTML = html;       
+    }
+
 }
-funCall()
+
+const tabel = new Tabel ({
+    judulTabel : ["Nama", "Email", "No.HP"],
+    data : [
+        ["bayu rizki", "baimempatlima@gmail.com", "08642134432"],
+        ["adi", "rizki@yahoo.com", "08653434324"]
+    ]
+});
+
+const tab = document.getElementById("tab");
+tabel.render(tab);
+
+
+
