c478b26a9e224baf8d003b29bb702f3f696a8f9a
baim empat <baimempatlima@gmail.com>
2022-01-09 09:25:13 +0700

    perbaikan Tas kFunction/tugasgame.js

--- Task Function/tugasgame.js
+++ Task Function/tugasgame.js
@@ -11,34 +11,40 @@ while (lagi) {
   let Acak = Math.floor(Math.random() * 3) + 1;
   console.log(Acak);
   // let berhenti = true
-  let cobalagi = cek(Pemain1, Pemain2);// misal false
- 
-
-  // Alur Permainan
   
-      if (!cobalagi) { // pasti true ok 
+ 
+  let cobalagi = cek(Pemain1, Pemain2);// misal false
+  // Alur Permainan
+    if (!cobalagi) { 
         lagi = confirm('Ulangi');
-      }else if (Pemain1 === Acak) {
-        alert('Tebakan Pemain 1  benar');
-        pemain1skor++;
-      }else  if (Pemain2 === Acak) {
-        alert('Tebakan Pemain 2 benar');
-        pemain2skor++;
-      }else{
-        alert('Tebakan Pemain 1 dan Pemain 2 salah');
-      }
-      alert('Skor Sementara \n' + 'Pemain 1 :' + pemain1skor + '\n' + 'Pemain 2 : ' + pemain2skor +'\n' + '\n' + 'Angka yang dicari: ' + Acak);
-    
-    match++;
-    if (match <= 5) {
-      lagi = confirm('Pertandingan Ke-' + match + '?');
     }else{
-      alert('Hasil Akhir \n' + 'Pemain 1 : ' + pemain1skor + '\n' + 'Pemain 2 : ' + pemain2skor );
-      lagi = confirm('Ulangi');
-    }
-    
+        if (Pemain1 !== Acak && Pemain2 !== Acak) {
+            console.log("Tebakan Pemain 1 dan Pemain 2 Benar");
+        }else{
+            if (Pemain1 == Acak) {
+                console.log("Pemain 1 Benar");
+                pemain1skor++;
+            }
+            if (Pemain2 == Acak) {
+                console.log("Pemain 2 Benar");
+                  pemain2skor++;
+                }
+            }
+            alert('Skor sementara \n Pemain 1 : ' + pemain1skor +"\n Pemain 2 : "+ pemain2skor + "\n Angka yang dicari : " + Acak); 
+            
+            match++;
+            if (match <= 5) {
+                lagi = confirm('Pertandingan Ke-' + match + '?');
+            }else{
+                alert('Pertandingan Berakhir Selamat');
+                match = 1;
+                pemain1skor = 0;
+                pemain2skor = 0;
+                lagi = confirm('Ulangi');
+            }
+        
+          }
 }
-
 
 function cek(Pemain1, Pemain2){
   if (Pemain1 === Pemain2) {
@@ -60,6 +66,5 @@ function cek(Pemain1, Pemain2){
     alert("salah satu player belum menebak angka");
     return false;
   }
-
   return true;
-};
+}
