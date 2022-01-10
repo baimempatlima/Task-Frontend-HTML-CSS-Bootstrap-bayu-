2351def923cf291f1ab19a7b4b8ad2cadf51cb80
baim empat <baimempatlima@gmail.com>
2022-01-10 20:49:51 +0700

    revisi module.js

--- Task Module System/module.js
+++ Task Module System/module.js
@@ -38,21 +38,5 @@ class Tabel {
   }
 
 }
-
-const tabel = new Tabel ({
-  judulTabel : ["Nama", "Email", "No.HP"],
-  data : [
-      ["bayu rizki", "baimempatlima@gmail.com", "08642134432"],
-      ["adi", "rizki@yahoo.com", "08653434324"]
-  ]
-});
-
-const tab = document.getElementById("tab");
-tabel.render(tab);
-
-
-
-
-  
-  export { tabel, tab };
+export { Tabel };
   
