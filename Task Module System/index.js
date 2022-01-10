91abb046e528b59418cf9eb57c9a068a05f5858a
baim empat <baimempatlima@gmail.com>
2022-01-10 12:42:00 +0700

    update task module system, index.js dan module.js

--- Task Module System/index.js
+++ Task Module System/index.js
@@ -1,3 +1,13 @@
-import { tabel, tab } from "./module";
+import {Tabel } from "./module";
 
+const tabel = new Tabel ({
+    judulTabel : ["Nama", "Email", "No.HP"],
+    data : [
+        ["bayu rizki", "baimempatlima@gmail.com", "08642134432"],
+        ["adi", "rizki@yahoo.com", "08653434324"]
+    ]
+  });
+  
+const tab = document.getElementById("tab");
 tabel.render(tab);
+
--- Task Module System/module.js
+++ Task Module System/module.js
@@ -1,55 +1,58 @@
 class Tabel {
-    constructor(awal) {
+  constructor(awal){
       this.awal = awal;
-    }
-  
-    headerTabel(data) {
-      var tagpembuka = "<thead><tr>";
-      var tagpenutup = "</tr></thead>";
-  
-      data.forEach((e) => {
-        tagpembuka += `<th>${e}</th>`;
-      });
-  
-      return tagpembuka + tagpenutup;
-    }
-  
-    bodyTabel(data) {
+  }
+
+  headerTabel(data) {
+     var tagpembuka = "<thead><tr>";  
+     var tagpenutup = "</tr></thead>";
+
+     data.forEach(e => {
+         tagpembuka += `<th>${e}</th>`;
+     });
+
+     return tagpembuka + tagpenutup;
+  }
+
+  bodyTabel(data){
       var tagpembuka = "<tbody>";
       var tagpenutup = "</tbody>";
-  
-      data.forEach((e) => {
-        tagpembuka += `
-            <tr>
-                <td>${e[0]}</td>
-                <td>${e[1]}</td>
-                <td>${e[2]}</td>
-            </tr>
-            `;
+
+      data.forEach((e)=>{
+              tagpembuka += `
+              <tr>`;
+                  for (var i = 0;  i < e.length; i++) {
+                      tagpembuka += `<td>${e[i]}</td>`;
+                  }
+               tagpembuka += `
+              </tr>`;          
+         
       });
       return tagpembuka + tagpenutup;
-    }
-  
-    render(elemen) {
-      var html =
-        "<table class='table table-bordered'>" +
-        this.headerTabel(this.awal.judulTabel) +
-        this.bodyTabel(this.awal.data) +
-        "</table>";
-      elemen.innerHTML = html;
-    }
   }
-  
-  const tabel = new Tabel({
-    judulTabel: ["Nama", "Email", "No.HP"],
-    data: [
+
+  render(elemen){
+      var html = 
+      "<table class='table table-bordered'>" + this.headerTabel(this.awal.judulTabel) + this.bodyTabel(this.awal.data) + "</table>";
+      elemen.innerHTML = html;       
+  }
+
+}
+
+const tabel = new Tabel ({
+  judulTabel : ["Nama", "Email", "No.HP"],
+  data : [
       ["bayu rizki", "baimempatlima@gmail.com", "08642134432"],
       ["adi", "rizki@yahoo.com", "08653434324"]
-    ]
-  });
-  
-  const tab = document.getElementById("tab");
-  // tabel.render(tab);
+  ]
+});
+
+const tab = document.getElementById("tab");
+tabel.render(tab);
+
+
+
+
   
   export { tabel, tab };
   
